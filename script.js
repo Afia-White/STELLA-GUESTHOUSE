425fd72f62578130b27a5b77c7bd1c25474e1bc0
Afia-White <afiawhite82@gmail.com>
2025-08-02 18:16:52 -0800

    NEW FIXES21

---
+++ script.js
@@ -0,0 +1,9 @@
+$('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      items:1,
+      autoplay: true,
+      autoplayTimeout: 3000, // 3 seconds — change this to whatever interval you want
+
+  });
